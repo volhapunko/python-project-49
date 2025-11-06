dd9bed37984c3ab7644a3dd585d9eb1e73cd618f
Volha <olgapunko1996@gmail.com>
2025-11-06 21:50:52 +0300

    amend brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,4 +1,5 @@
 import prompt
+
 from brain_games.cli import welcome_user
 from brain_games.games.even import generate_number
 
@@ -6,8 +7,6 @@ from brain_games.games.even import generate_number
 def main():
     name = welcome_user()
     print('Answer "yes" if the number is even, otherwise answer "no".')
-
-
     rounds_to_win = 3
 
     for round in range(rounds_to_win):
@@ -16,7 +15,8 @@ def main():
         user_answer = prompt.string('Your answer: ')
 
         if user_answer != correct_answer:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"'{user_answer}' is wrong answer ;(."
+                f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             return
     
