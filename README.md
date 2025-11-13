10ea1df1bc256ccb5d5a9aec7acd1126a4d4599d
Volha <olgapunko1996@gmail.com>
2025-11-13 22:58:10 +0300

    add new game progression

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/volhapunko/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/volhapunko/python-project-49/actions)
 https://asciinema.org/a/pLSxzIdyPHOlpdbdpDvP0tLYP
+https://asciinema.org/a/yc5Kwt7mvPxUz79FP4OsJuhz9
---
+++ brain_games/games/progression.py
@@ -0,0 +1,29 @@
+from random import randint
+
+GAME_DESCRIPTION = 'What number is missing in the progression?'
+
+
+def generate_progression():
+    start = randint(1, 10)
+    length = randint(5, 10)
+    step = randint(1, 5)
+    
+    progression = []
+    for element in range(length):
+        current_element = start + element * step
+        progression.append(current_element)
+    return (progression)
+
+
+def generate_number():
+    progression = generate_progression()
+    skip_number = randint(0, len(progression) - 1)
+    question = []
+    for i in range(len(progression)):
+        if i == skip_number:
+            question.append('..')
+        else:
+            question.append(str(progression[i]))
+    
+    correct_answer = progression[skip_number]
+    return ' '.join(question), str(correct_answer)
